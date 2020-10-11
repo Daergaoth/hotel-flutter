e1ab60c7090c7833760a0ae8e7b1c5ce5d308ecb
Segyevy Nándor <segyevy.nandor@gmail.com>
2020-10-11 11:55:25 +0200

    date pickers + validation

--- progmasters_hotel/lib/pages/home_page.dart
+++ progmasters_hotel/lib/pages/home_page.dart
@@ -8,52 +8,60 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   Map cityNames;
+  DateTime _datePickerStartMax;
+  DateTime _currentDate;
+  DateTime _start;
+  DateTime _end;
+  DateTime _endMinValue;
+  List<String> _suggestions;
+  String _selectedCity;
+  AutoCompleteTextField _searchField;
+  GlobalKey<AutoCompleteTextFieldState<String>> key;
+  String _pickedStartDate;
+  String _pickedEndDate;
+  bool _isDateInvalid;
+  bool _isCityNameInvalid;
 
-  // Widget row(String cityName){
-  //   return Row(
-  //     mainAxisAlignment: MainAxisAlignment.spaceBetween,
-  //     children: <Widget>[
-  //       Text(cityName,
-  //       style: TextStyle(
-  //         color: Colors.lightBlue[900],
-  //         fontSize: 20.0
-  //       ),
-  //       ),
-  //       SizedBox(
-  //         width: 10.0,
-  //       )
-  //     ],
-  //   );
-  // }
+  initState() {
+    super.initState();
+    _currentDate = DateTime.now();
+    _datePickerStartMax =
+        DateTime((_currentDate.year + 1), _currentDate.month, _currentDate.day);
+    key = new GlobalKey();
+    _start =
+        DateTime(_currentDate.year, _currentDate.month, _currentDate.day, 2);
+    _pickedStartDate = '${_start.year} - ${_start.month} - ${_start.day}';
+    _end = setupEndDate(_start);
+    _pickedEndDate = '${_end.year} - ${_end.month} - ${_end.day}';
+    _isDateInvalid = false;
+    _isCityNameInvalid = false;
+    _endMinValue = _end;
+  }
 
   @override
   Widget build(BuildContext context) {
     cityNames = ModalRoute.of(context).settings.arguments;
-    List<String> suggestions = cityNames['cityNames'];
-    print(cityNames);
-    String selectedCity;
+    _suggestions = cityNames['cityNames'];
 
-    AutoCompleteTextField searchField;
-    GlobalKey<AutoCompleteTextFieldState<String>> key = new GlobalKey();
-
-    searchField = AutoCompleteTextField(
+    _searchField = AutoCompleteTextField(
         decoration: InputDecoration(
             // errorText: 'Field is mandatory',
             contentPadding: EdgeInsets.fromLTRB(24.0, 12.0, 24.0, 12.0),
             hintText: "Enter city you want to visit"),
         itemSubmitted: (item) {
           setState(() {
-            searchField.textField.controller.text = item;
+            _searchField.textField.controller.text = item;
           });
         },
         key: key,
         textChanged: (text) {
-          selectedCity = text;
+          _selectedCity = text;
+          validateCityName(text);
         },
         textSubmitted: (text) {},
         clearOnSubmit: false,
         submitOnSuggestionTap: false,
-        suggestions: suggestions,
+        suggestions: _suggestions,
         itemBuilder: (context, item) {
           return Text(
             item,
@@ -84,14 +92,97 @@ class _HomeState extends State<Home> {
         ),
         body: Column(
           children: <Widget>[
-            searchField,
             Padding(
-              padding: const EdgeInsets.fromLTRB(0.0, 45.0, 0.0, 12.0),
+              padding: const EdgeInsets.all(24.0),
+              child: _searchField,
+            ),
+            Text(
+              _isCityNameInvalid ? 'City name is mandatory' : '',
+              style: TextStyle(
+                color: Colors.red[700],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text('From: $_pickedStartDate'),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: IconButton(
+                        icon: Icon(Icons.calendar_today),
+                        onPressed: () {
+                          showDatePicker(
+                              context: context,
+                              initialDate: _start,
+                              firstDate: _currentDate,
+                              lastDate: _datePickerStartMax)
+                              .then((date) {
+                            setState(() {
+                              if (date != null) {
+                                _start = date;
+                                _pickedStartDate =
+                                '${_start.year} - ${_start.month} - ${_start
+                                    .day}';
+                                validateStartDate();
+                                if (!_isDateInvalid) {
+                                  _endMinValue = setupEndDate(_start);
+                                }
+                              }
+                            });
+                          });
+                        }),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text('Till: $_pickedEndDate'),
+                  ),
+                  IconButton(
+                      icon: Icon(Icons.calendar_today),
+                      // child: Text('Select a end date.'),
+                      onPressed: () {
+                        showDatePicker(
+                            context: context,
+                            initialDate: _end,
+                            firstDate: _endMinValue,
+                            lastDate: _datePickerStartMax)
+                            .then((date) {
+                          setState(() {
+                            if (date != null) {
+                              _end = date;
+                              _pickedEndDate =
+                              '${_end.year} - ${_end.month} - ${_end.day}';
+                              validateStartDate();
+                            }
+                          });
+                        });
+                      }),
+                ],
+              ),
+            ),
+            Text(
+              _isDateInvalid
+                  ? 'Date is invalid, please check it. \nMax reservation duration is 28 days.'
+                  : '',
+              style: TextStyle(
+                color: Colors.red[700],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(0.0, 45.0, 0.0, 0.0),
               child: IconButton(
                 icon: Icon(Icons.search),
                 onPressed: () {
-                  print(selectedCity);
-                  print('sajt');
+                  print(_selectedCity);
                 },
                 color: Colors.lightBlue[900],
                 iconSize: 36.0,
@@ -100,5 +191,58 @@ class _HomeState extends State<Home> {
           ],
         ));
     //throw UnimplementedError();
+  }
+
+  void validateStartDate() {
+    int difference = _end
+        .difference(_start)
+        .inDays;
+    if (difference < 1 || difference > 28) {
+      _isDateInvalid = true;
+    } else {
+      _isDateInvalid = false;
+    }
+  }
+
+  DateTime setupEndDate(DateTime start) {
+    int startYear = start.year;
+    int startMonth = start.month;
+    int startDay = start.day;
+
+    DateTime endToReturn;
+
+    if ((startMonth == 1 ||
+        startMonth == 3 ||
+        startMonth == 5 ||
+        startMonth == 7 ||
+        startMonth == 8 ||
+        startMonth == 10) &&
+        startDay == 31) {
+      endToReturn = DateTime(startYear, startMonth + 1, 1);
+    } else if (startMonth == 12 && startDay == 31) {
+      endToReturn = DateTime(startYear + 1, 1, 1);
+    } else if ((startMonth == 4 ||
+        startMonth == 6 ||
+        startMonth == 9 ||
+        startMonth == 11) &&
+        startDay == 30) {
+      endToReturn = DateTime(startYear, startMonth + 1, 1);
+    } else if (startYear % 4 == 0 && startMonth == 2 && startDay == 29) {
+      endToReturn = DateTime(startYear, startMonth + 1, 1);
+    } else if (startYear % 4 != 0 && startMonth == 2 && startDay == 28) {
+      endToReturn = DateTime(startYear, startMonth + 1, 1);
+    } else {
+      endToReturn = DateTime(startYear, startMonth, startDay + 1);
+    }
+
+    return endToReturn;
+  }
+
+  void validateCityName(String text) {
+    if (text == null) {
+      _isCityNameInvalid = true;
+    } else {
+      _isCityNameInvalid = false;
+    }
   }
 }
